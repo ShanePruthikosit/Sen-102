22675bc4eb06ab3c63106113d046999fba293921
Trollgodlol <86240535+Trollgodlol@users.noreply.github.com>
2023-09-28 21:34:55 +0700

    lab 1 done

--- Lab1/circle.c
+++ Lab1/circle.c
@@ -1,27 +1,61 @@
+// Circle.c
+//
+// Lab 1 program that calculates 
+// the circumference, area and the Volume of a sphere 
+// created by the circle when given the radius
+//
+// Created by shane on 26/9/2023
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
+// Calculates the area when given the radius
+// Arguments:
+//      radius - radius given by the user
+// assumes that the input is valid
 float findArea(int radius)
 {
     float Area = 3.14*(pow(radius,2));
     return Area;
 }
 
+// Calculates the circumference when given the radius
+// Arguments:
+//      radius - radius given by the user
+// assumes that the input is valid
 float findCircumference(int radius)
 {
     float length = 3.14*2*radius;
     return length;
 }
 
+// Calculates the volume when given the radius
+// Arguments:
+//      radius - radius given by the user 
+// assumes that the input is valid
 float findSphereVolume(int radius)
 {
     float Area = (4/3)*3.14*(pow(radius,3));
     return Area;
 }
 
-void validateInput(int radius){
-
+// checks if the user's input is valid or not
+// Arguments:
+//      input - input typed in by the user stored as string
+// assumes that the input is a string
+void validateInput(char input[]){
+    int len = strlen(input)-1;
+    for(int letterNumber = 0; letterNumber < len; letterNumber++)
+    {
+        if (!isdigit(input[letterNumber]))
+        {
+            printf("%s", "Please Input a valid positive Integer");
+            exit(1);
+        }
+    }
 }
 
 int main()
@@ -30,14 +64,8 @@ int main()
     char inputline[32];
     printf("Enter value for radius (whole number): ");
     fgets(inputline,sizeof(inputline),stdin);
+    validateInput(inputline);
     sscanf(inputline,"%d",&radius);
-    TRY{
-        validateInput(radius);
-    }
-    CATCH{
-        printf("stop");
-        exit(1);
-    }
     printf("Circumference: %.1f \n",findCircumference(radius));
     printf("Area: %.1f \n",findArea(radius));
     printf("Volume: %.1f \n",findSphereVolume(radius));
